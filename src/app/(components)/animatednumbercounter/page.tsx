c946a6879245160d3c101bc448b89bd00c8f6cde
Ayush tomar <ayushtomar388@gmail.com>
2024-10-25 22:18:51 +0530

    Animated Number Counter created

--- src/app/(components)/animatednumbercounter/page.tsx
+++ src/app/(components)/animatednumbercounter/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+import { useSpring } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const number = 60;
+
 const AnimatedNumberCounter = () => {
+
+    const [displayNumber, setDisplayNumber] = useState(0);
+
+    const springNumberCount = useSpring(0, {
+        bounce: 0,
+        duration: 10000,
+    });
+
+    springNumberCount.on('change', (value) => {
+        setDisplayNumber(Math.round(value));
+    })
+
+    useEffect(() => {
+        springNumberCount.set(number)
+    }, []);
+
     return ( 
-        <div>
-            Animated Number Counter
+        <div className="flex items-center justify-center h-screen text-6xl font-bold ">
+            {displayNumber}
         </div>
      );
 }
