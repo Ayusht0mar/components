2452e408bfc1cdf32f5f53db800b70c34dd15476
Ayush tomar <ayushtomar388@gmail.com>
2024-11-11 21:36:11 +0530

    Grid Bg effect created

---
+++ src/app/(components)/gridbg/page.tsx
@@ -0,0 +1,8 @@
+const GridBg = () => {
+    return ( 
+        <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-100 bg-[linear-gradient(to_right,#d4d4d4_1px,transparent_1px),linear-gradient(to_bottom,#d4d4d4_1px,transparent_1px)] bg-[size:32px_32px]">
+        </div>
+     );
+}
+ 
+export default GridBg;
