2abba09a360ff0cb4c5cf6cd9f52ab41406a48e1
Ayush tomar <ayushtomar388@gmail.com>
2024-11-14 23:43:12 +0530

    Pricing Section created

---
+++ src/app/(components)/pricingsection/page.tsx
@@ -0,0 +1,12 @@
+const PricingSection = () => {
+    return ( 
+        <div className="mx-auto py-20 text-center">
+            <h2 className="text-6xl font-bold sm:text-7xl">Pricing</h2>
+            <p className="mt-5 text-gray-600 sm:text-lg">Try for free. Get premium when you feel the time is right.</p>
+
+
+        </div>
+     );
+}
+ 
+export default PricingSection;
