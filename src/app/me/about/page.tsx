894852212f3fda169abdf0d68a24826c8f361177
Ayush tomar <ayushtomar388@gmail.com>
2024-11-15 22:25:37 +0530

    About pape created

---
+++ src/app/me/about/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/app/(components)/navbar5/page";
+
+const AboutPage = () => {
+    return ( 
+        <div>
+            About
+            <Navbar/>
+        </div>
+     );
+}
+ 
+export default AboutPage;
