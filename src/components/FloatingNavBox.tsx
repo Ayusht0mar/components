e59b7e713ba24730a0b2dd678664ade5e9d65a3e
Ayush tomar <ayushtomar388@gmail.com>
2024-11-22 23:37:49 +0530

    FloatingNavBox component created

---
+++ src/components/FloatingNavBox.tsx
@@ -0,0 +1,9 @@
+const FloatingNavBox = () => {
+    return ( 
+        <div className="fixed bottom-2 right-2 text-white bg-black py-2 px-4">
+            Navbar
+        </div>
+     );
+}
+ 
+export default FloatingNavBox;
