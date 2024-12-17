b1afb1e1419189ca5e480b0195eaf8a9f9e80227
Ayush tomar <ayushtomar388@gmail.com>
2024-12-17 23:23:29 +0530

    Compare Image component made workable

--- src/app/(components)/compareimages/page.tsx
+++ src/app/(components)/compareimages/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { GripVertical } from "lucide-react"
+import Image from "next/image"
+import * as ResizablePrimitive from "react-resizable-panels"
+
+
 interface CompareImagesProps {
     containerClassName? : string
 }
@@ -6,12 +13,31 @@ const CompareImages = (
     {containerClassName}: CompareImagesProps
 ) => {
     return ( 
-        <div className=" grid place-content-center h-screen">
-            <div className={`bg-black relative flex justify-center ${containerClassName}`}>
-                <h2 className="text-white">Image 1</h2>
-                <button className="absolute w-1 h-full top-0 bg-slate-500 overflow-hidden">Button</button>
-                <h2 className="text-white">Image 2</h2>
-            </div>
+        <div className="w-[800px] h-96">
+            <ResizablePrimitive.PanelGroup direction="horizontal" className="w-96 mx-auto">
+                <ResizablePrimitive.Panel className="bg-blue-400 overflow-hidden relative" defaultSize={50}>
+                        <Image
+                            src="/agency.webp"
+                            alt="Image"
+                            fill
+                            style={{objectFit: "cover", objectPosition: "left"}}
+                            className="absolute"
+                        />
+                </ResizablePrimitive.Panel>
+                <ResizablePrimitive.PanelResizeHandle className="relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90">
+                    <div className="z-10 absolute flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+                        <GripVertical className="h-2.5 w-2.5" />
+                    </div>  
+                </ResizablePrimitive.PanelResizeHandle>
+                <ResizablePrimitive.Panel className="bg-black overflow-hidden relative" defaultSize={50}>
+                <Image
+                        src="/candy.webp"
+                        alt="Image"
+                        fill
+                        style={{objectFit: "cover", objectPosition: "right"}}	
+                    />
+                </ResizablePrimitive.Panel>
+            </ResizablePrimitive.PanelGroup>    
         </div>
      );
 }
