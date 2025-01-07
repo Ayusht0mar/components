61b596f2ecd857ec363aab3dce7178babc86db03
Ayush tomar <ayushtomar388@gmail.com>
2025-01-07 23:58:01 +0530

    kanbanboard page created

---
+++ src/app/(components)/kanbanboard/page.tsx
@@ -0,0 +1,11 @@
+import { KanbanBoard } from "@/components/KanbanBoard";
+
+const KanbanBoardPage = () => {
+    return ( 
+        <div>
+                <KanbanBoard />
+        </div>
+     );
+}
+ 
+export default KanbanBoardPage;
