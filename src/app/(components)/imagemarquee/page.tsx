fa7cfd22fd25e3051eebe9a25d7af74b814bcab3
Ayush tomar <ayushtomar388@gmail.com>
2025-01-19 17:44:37 +0530

    imagemarquee component created

---
+++ src/app/(components)/imagemarquee/page.tsx
@@ -0,0 +1,12 @@
+import ImageMarquee from "@/components/ImageMarquee/ImageMarquee";
+
+const ImageMarqueePage = () => {
+    return ( 
+        <div>
+            <ImageMarquee/>
+            
+        </div>
+     );
+}
+ 
+export default ImageMarqueePage;
