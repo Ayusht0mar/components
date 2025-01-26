fed5cdd697df5defe52ec6e5649e88d9b5f5e025
Ayush tomar <ayushtomar388@gmail.com>
2025-01-26 22:27:09 +0530

    Image Marquee completed

--- src/components/ImageMarquee/ImageMarquee.tsx
+++ src/components/ImageMarquee/ImageMarquee.tsx
@@ -1,22 +1,27 @@
 "use client"
 import useMeasure from "react-use-measure";
-import Card from "./Card";
 import { animate, useMotionValue } from "framer-motion";
 import { useEffect } from "react";
 import {motion} from "framer-motion";
+import Image from "next/image";
 
 
 const ImageMarquee = () => {
 
     const images = [
-        "/image-1.jpg",
-        "/image-2.jpg",
-        "/image-3.jpg",
-        "/image-4.jpg",
-        "/image-5.jpg",
-        "/image-6.jpg",
-        "/image-7.jpg",
-        "/image-8.jpg",
+        "/logos/apple.svg",
+        "/logos/discord.svg",
+        "/logos/facebook.svg",
+        "/logos/figma.svg",
+        "/logos/google.svg",
+        "/logos/instagram.svg",
+        "/logos/linkedin.svg",
+        "/logos/spotify.svg",
+        "/logos/tictok.svg",
+        "/logos/twitch.svg",
+        "/logos/whatsapp.svg",
+        "/logos/x.svg",
+        "/logos/youtube.svg",
     ]
 
     let [ref, {width}] = useMeasure();
@@ -25,11 +30,11 @@ const ImageMarquee = () => {
     
     useEffect(() => {
         let controls;
-        let FinalPosition = -width/2 -6;
+        let FinalPosition = -width/3 - 8;
 
         controls = animate(xTraslation, [0,FinalPosition], {
             ease: 'linear',
-            duration: 20,
+            duration: 12,
             repeat: Infinity,
             repeatType: "loop",
             repeatDelay:0
@@ -39,9 +44,9 @@ const ImageMarquee = () => {
     }, [xTraslation, width])
 
     return ( 
-        <div className="relative py-2 w-[50vw] mx-auto h-60 overflow-hidden">
-            <motion.div className="absolute left-0 top-4 flex gap-4" ref={ref} style={{x: xTraslation}}>
-                {[...images, ...images].map((item, idx) => (
+        <div className="relative py-2 mx-auto h-60 overflow-hidden">
+            <motion.div className="absolute left-0 top-4 flex gap-8" ref={ref} style={{x: xTraslation}}>
+                {[...images, ...images, ...images].map((item, idx) => (
                     <Card key={idx} image={item} />
                 ))}
 
@@ -52,3 +57,22 @@ const ImageMarquee = () => {
 }
  
 export default ImageMarquee;
+
+
+interface CardProps {
+    image: string;
+}
+
+const Card: React.FC<CardProps> = ({image}) => {
+    return ( 
+        <motion.div className="relative overflow-hidden size-12 rounded-xl flex justify-center items-center">
+            <Image
+                src={image}
+                alt={image}
+                fill 
+                style={{objectFit:'cover'}}
+            />
+        </motion.div>
+     );
+}
+ 
